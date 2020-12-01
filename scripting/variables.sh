c9cea57096bea04a0773251be3475f560913f06c
shashiofficial <shashiofficial10@gmail.com>
2020-12-01 12:08:21 +0530

    Added variables

---
+++ scripting/variables.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/bash
+
+# This is a comment
+
+echo "Hello world" # Print/display data on terminal
+
+# Pre defined variables (They are mostly upper case)
+
+echo $BASH
+echo Shell version is $BASH_VERSION
+echo Home directory is $HOME
+echo Curreny working directory is $PWD
+
+# User defined variables (They are mostly lower case)
+
+name=Shashi #Variable name must not start with a letter
+age=20 
+echo Name is $name
+echo Age is $age
