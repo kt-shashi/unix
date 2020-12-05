e025b513e6bbde134661385f61a6008b2aeb7b7b
shashiofficial <shashiofficial10@gmail.com>
2020-12-05 12:21:21 +0530

    Added arithmetic operations (Integers)

---
+++ scripting/arithmeticop.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/bash
+
+num1=20
+num2=5
+
+echo $(( $num1 + $num2 ))
+echo $(( $num1 - $num2 ))
+echo $(( $num1 * $num2 ))
+echo $(( $num1 / $num2 ))
+echo $(( $num1 % $num2 ))
+
+echo $(expr $num1 + $num2 )
+echo $(expr $num1 - $num2 )
+echo $(expr $num1 \* $num2 )
+echo $(expr $num1 / $num2 )
+echo $(expr $num1 % $num2 )
